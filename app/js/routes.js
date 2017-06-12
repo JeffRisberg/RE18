e876de968e2c2488395a2300cfd39366638df0ac
JeffRisberg <jrisberg@aol.com>
2017-06-11 22:01:10 -0700

    created navigation

--- app/js/routes.js
+++ app/js/routes.js
@@ -2,10 +2,7 @@ import React from "react";
 import {Route, IndexRoute} from "react-router";
 import AppRoot from "./AppRoot";
 import Home from "./components/Home";
-import ItemListContainer from "./components/Items/ItemListContainer";
-import ItemFormContainer from "./components/Items/ItemFormContainer";
-import EventListContainer from "./components/Events/EventListContainer";
-import EventFormContainer from "./components/Events/EventFormContainer";
+import SetList from "./pages/SetList";
 
 export default (
     <Route path="/" component={AppRoot}>
